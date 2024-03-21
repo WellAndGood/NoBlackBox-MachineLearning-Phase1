a4aaacbb0e1e652b86d19838abdaf2e3b6a26563
WellAndGood <danielpisani12@hotmail.com>
2024-03-20 23:58:24 -0400

    complete tutorial. Decision matrix with interactive images and live-categorization

--- common/utils.js
+++ common/utils.js
@@ -12,6 +12,7 @@ utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
         pencil: {color: 'magenta', text: '✏️'},
         clock:  {color: 'lightgrey', text: '🕒'},
     };
+utils.styles["?"] = {color: 'red', text: '❓'};
 
 utils.formatPercent = (n) => {
     return (n*100).toFixed(2) + "%";
@@ -40,6 +41,68 @@ utils.groupBy = (objArray, key) => {
     return groups;
 }
 
-if (typeof window !== undefined) {
-    window.exports=utils;
+utils.distance=(p1,p2)=>{
+    return Math.sqrt(
+       (p1[0]-p2[0])**2+
+       (p1[1]-p2[1])**2
+    );
+ };
+ 
+ utils.getNearest=(loc,points, k=1)=>{
+    const obj =points.map((val, ind) => {
+        return {ind, val}
+    });
+    const sorted = obj.sort((a, b) => {
+        return utils.distance(loc, a.val) - utils.distance(loc, b.val)
+    });
+    const indices = sorted.map((obj) => obj.ind);
+    return indices.slice(0, k);
+
+
+    // let minDist=Number.MAX_SAFE_INTEGER;
+    // let nearestIndex=0;
+ 
+    // for(let i=0;i<points.length;i++){
+    //    const point=points[i];
+    //    const d=math.distance(loc,point);
+ 
+    //    if(d<minDist){
+    //       minDist=d;
+    //       nearestIndex=i;
+    //    }
+    // }
+    // return nearestIndex;
+ }
+
+ utils.invLerp = (a, b, v) => {
+    return (v-a) / (b-a);
+ }
+
+ utils.normalizePoints = (points, minMax) => {
+    let min, max;
+    const dimensions = points[0].length;
+    if (minMax) {
+        min=minMax.min;
+        max=minMax.max;
+    } else {
+        min=[...points[0]];
+        max=[...points[0]];
+        for (let i=1; i< points.length; i++) {
+            for (let j=0; j<dimensions; j++) {
+                min[j] = Math.min(min[j], points[i][j]);
+                max[j] = Math.max(max[j], points[i][j]);
+            }
+        }
+    }
+    for (let i=0; i < points.length; i++) {
+        for (let j=0; j<dimensions; j++) {
+            points[i][j] =
+                utils.invLerp(min[j], max[j], points[i][j])
+        }
+    }
+    return {min, max}
+ }
+
+if (typeof module !== 'undefined') {
+    module.exports=utils;
 }
